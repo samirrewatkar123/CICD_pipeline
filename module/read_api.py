c5fa31137b88bd0ac0703742f7eb9ebb352be5e7
harendra263 <harendra263@gmail.com>
2024-07-12 12:25:30 +0530

    changes to module folder

--- module/read_api.py
+++ module/read_api.py
@@ -6,10 +6,9 @@ pd.set_option("display.max_columns", 100)
 #2. https://reqres.in/
 #3. https://dummyapis.com/
 
+api = "https://hub.dummyapis.com/employee?noofRecords=10&idStarts=1001"
 
-response = requests.get("https://hub.dummyapis.com/employee?noofRecords=10&idStarts=1001", verify=False)
-
-data = response.json()
-
-df = pd.DataFrame(data=data)
-print(df.head(5))
+def read_api(url: str = api) ->pd.DataFrame:
+    response = requests.get(url, verify=False)
+    data = response.json()
+    return pd.DataFrame(data=data)
