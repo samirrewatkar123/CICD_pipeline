14090f6a286a28271802d4d61258ebd060104ee4
Samir Vilasrao Revatkar <revatkar.5@TeleperformanceDIBS.com>
2024-07-16 17:12:58 +0530

    updated_change

--- tests/test_math_operations.py
+++ tests/test_math_operations.py
@@ -4,13 +4,19 @@ import os
 # Add the module directory to the sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'module')))
 
-from module import addition
+
+
+from module import addition, subtact
 
 
 
 def test_add_two_numbers() -> None:
-    assert addition.add_two_numbers(a=1, b=2) == 3
+    assert addition.add_two_numbers(a=-2, b=2) == 0
     assert addition.add_two_numbers(-1, 1) == 0
     assert addition.add_two_numbers(0, 0) == 0
     assert addition.add_two_numbers(-5, -5) == -10
     assert addition.add_two_numbers(100, 200) == 300
+    
+    
+def sub_two_number() -> None:
+    assert subtact.sub_two_numbers(a=2, b=2) == 0
